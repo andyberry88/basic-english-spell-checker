e9ed59debd5a1ee1047f64675cfb65f32fd6dd8b
Andy Berry <andyberry88@gmail.com>
2019-04-15 09:30:16 +0100

    add tests to validate no response for all correct words

--- src/spell-checker.test.js
+++ src/spell-checker.test.js
@@ -6,4 +6,18 @@ describe('spell checker', () => {
     it('should export a function', () => {
         expect(spellChecker).to.be.a('function');
     });
+
+    context('when there are no errors', () => {
+        it('should validate a single word as correct', () => {
+            expect(spellChecker('come')).to.deep.equal([]);
+        });
+
+        it('should validate 2 words as correct', () => {
+            expect(spellChecker('come account')).to.deep.equal([]);
+        });
+
+        it('should validate many words as correct', () => {
+            expect(spellChecker('come account able bent cold take')).to.deep.equal([]);
+        });
+    });
 });
